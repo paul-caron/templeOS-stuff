d6e89ba49c299721b64c7460f7d17eef2e63cc98
Paul Caron <paul.a.caron@gmail.com>
2024-08-05 18:11:27 -0500

    qemu sound now working

--- qemu/run.sh
+++ qemu/run.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -audio driver=coreaudio,model=virtio -drive format=raw,file=hardDrive.img -m 512M
+qemu-system-x86_64 -audiodev coreaudio,id=snd0 -machine pcspk-audiodev=snd0 -drive format=raw,file=hardDrive.img -m 512M
