83b7458334fc83c56f8acc57072d20fe4b880b15
Paul Caron <paul.a.caron@gmail.com>
2024-08-09 19:17:56 -0500

    beep

--- qemu/run.sh
+++ qemu/run.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -audiodev coreaudio,id=snd0 -machine pcspk-audiodev=snd0 -drive format=raw,file=hardDrive.img -m 512M
+qemu-system-x86_64 -cdrom TOS_Supplemental3.ISO.C -audiodev coreaudio,id=snd0 -machine pcspk-audiodev=snd0 -drive format=raw,file=hardDrive.img -m 512M
